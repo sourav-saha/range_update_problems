7ce3996ac9636db0709efebf1d7a1ad3885333f6
sourav-saha <sourav.saha048@gmail.com>
2022-01-17 22:52:38 +0530

    Update meeting_room_I.py

--- meeting_room_I.py
+++ meeting_room_I.py
@@ -1,4 +1,6 @@
 '''
+https://www.lintcode.com/problem/920
+
 Description
 Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
 
